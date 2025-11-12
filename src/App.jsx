a79d5c8e063cd6c70a30fbbedb41fd201651fbb8
katerynahoryslavets <kateryna.horyslavets@gmail.com>
2025-11-12 16:46:01 +0200

    Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import Home from "./pages/Home";
-import Tarots from "./pages/Tarots";
-import WaiteTarotMeaning from "./pages/Waite-tarot";
-import CeccoliTarotMeaning from "./pages/Ceccoli-tarot";
+import WaiteTarotPage from "./pages/Waite-tarot";
+import CeccoliTarotPage from "./pages/Ceccoli-tarot";
 import Cups from "./pages/Cups";
 import Pentacles from "./pages/Pentacles";
 import Swords from "./pages/Swords";
@@ -17,12 +16,11 @@ export default function App() {
         <Router>
             <div className="flex flex-col min-h-screen bg-gradient-to-b from-purple-50 to-purple-100">
                 <Navbar />
-                <main className="flex-grow p-8">
+                <main className="flex-grow">
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/tarots" element={<Tarots />} />
-                        <Route path="/waite" element={<WaiteTarotMeaning/>} />
-                        <Route path="/ceccoli" element={<CeccoliTarotMeaning/>} />
+                        <Route path="/waite" element={<WaiteTarotPage/>} />
+                        <Route path="/ceccoli" element={<CeccoliTarotPage/>} />
                         <Route path="/questions" element={<Questions/>} />
                         <Route path="/cups" element={<Cups />} />
                         <Route path="/pentacles" element={<Pentacles />} />
